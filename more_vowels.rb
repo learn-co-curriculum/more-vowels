5d61fb250e6971ed56a1e463e4e356fc89f37f7f
Logan Hasson <logan.hasson@gmail.com>
2014-01-14 17:39:51 -0500

    First two solutions

--- more_vowels.rb
+++ more_vowels.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+
+def vowels_with_if_elsif(letter)
+  if letter == 'a'
+    true
+  elsif letter == 'e'
+    true
+  elsif letter == 'i'
+    true
+  elsif letter == 'o'
+    true
+  elsif letter == 'u'
+    true
+  else
+    false
+  end
+end
+
+def vowels_with_case(letter)
+  case letter
+  when 'a'
+    true
+  when 'e'
+    true
+  when 'i'
+    true
+  when 'o'
+    true
+  when 'u'
+    true
+  else
+    false
+  end
+end
